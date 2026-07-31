b09ae4b3d7062340951aee3d92823c332c0b6007
Phi Long <shamt2004@gmail.com>
2026-07-31 22:48:57 +0700

    feat: fetch YouTube's own Japanese caption track via the timedtext endpoint

--- lib/youtube/index.ts
+++ lib/youtube/index.ts
@@ -1,3 +1,4 @@
 export { parseVideoId } from "./video-id";
 export { fetchOembed, OembedFetchError, type OembedResult } from "./oembed";
 export { importVideoSchema, type ImportVideoInput } from "./schema";
+export { fetchJapaneseCaptions, type TimedTextLine } from "./timedtext";
